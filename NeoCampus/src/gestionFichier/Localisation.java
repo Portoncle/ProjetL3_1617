972f03b764f6b607e8526cbe36704df0ceb13e38
NoemiePetitcol <noemie.petitcol@gmail.com>
2016-11-27 17:11:59 +0100

    test

--- NeoCampus/src/gestionFichier/Localisation.java
+++ NeoCampus/src/gestionFichier/Localisation.java
@@ -14,10 +14,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.NavigableSet;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class Localisation {
     
