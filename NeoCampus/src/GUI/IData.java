731cf057fbd5e31c73165536a880155c1e64133f
Portoncle <nicolas.palacios@hotmail.fr>
2016-12-05 18:15:05 +0100

    Avancement IData

--- NeoCampus/src/GUI/IData.java
+++ NeoCampus/src/GUI/IData.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import javax.swing.JOptionPane;
+import ressources.PositionCapteurExt;
 
 /**
  * Interface recuperants les premieres informations
@@ -8,6 +9,8 @@ import javax.swing.JOptionPane;
  */
 public class IData extends javax.swing.JFrame {
 
+    PositionCapteurExt capteurExt;
+    
     private boolean checkOK() {
         // Identificateur
         if(jTextFieldID.getText().equals("")) {
@@ -32,6 +35,19 @@ public class IData extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jDialogGPS = new javax.swing.JDialog();
+        jPanelGPS = new javax.swing.JPanel();
+        jLabelLat = new javax.swing.JLabel();
+        jTextFieldLat = new javax.swing.JTextField();
+        jLabelLong = new javax.swing.JLabel();
+        jTextFieldLong = new javax.swing.JTextField();
+        jPanelBlank2 = new javax.swing.JPanel();
+        jButtonGPSOK = new javax.swing.JButton();
+        jDialogInter = new javax.swing.JDialog();
+        jPanelInterMain = new javax.swing.JPanel();
+        jScrollPaneInterTree = new javax.swing.JScrollPane();
+        jTreeInter = new javax.swing.JTree();
+        jButtonInterOK = new javax.swing.JButton();
         jPanelMain = new javax.swing.JPanel();
         jLabelID = new javax.swing.JLabel();
         jTextFieldID = new javax.swing.JTextField();
@@ -49,6 +65,78 @@ public class IData extends javax.swing.JFrame {
         jLabelUnit2 = new javax.swing.JLabel();
         jPanelBlank = new javax.swing.JPanel();
         jButtonNext = new javax.swing.JButton();
+
+        jDialogGPS.setTitle("Coordonnées GPS");
+        jDialogGPS.setAlwaysOnTop(true);
+        jDialogGPS.setMinimumSize(new java.awt.Dimension(230, 150));
+
+        jPanelGPS.setMinimumSize(new java.awt.Dimension(0, 0));
+        jPanelGPS.setLayout(new java.awt.GridLayout(3, 2));
+
+        jLabelLat.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelLat.setText("Latitude");
+        jPanelGPS.add(jLabelLat);
+        jPanelGPS.add(jTextFieldLat);
+
+        jLabelLong.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelLong.setText("Longitude");
+        jPanelGPS.add(jLabelLong);
+        jPanelGPS.add(jTextFieldLong);
+
+        javax.swing.GroupLayout jPanelBlank2Layout = new javax.swing.GroupLayout(jPanelBlank2);
+        jPanelBlank2.setLayout(jPanelBlank2Layout);
+        jPanelBlank2Layout.setHorizontalGroup(
+            jPanelBlank2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 115, Short.MAX_VALUE)
+        );
+        jPanelBlank2Layout.setVerticalGroup(
+            jPanelBlank2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        jPanelGPS.add(jPanelBlank2);
+
+        jButtonGPSOK.setText("OK");
+        jButtonGPSOK.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonGPSOKActionPerformed(evt);
+            }
+        });
+        jPanelGPS.add(jButtonGPSOK);
+
+        javax.swing.GroupLayout jDialogGPSLayout = new javax.swing.GroupLayout(jDialogGPS.getContentPane());
+        jDialogGPS.getContentPane().setLayout(jDialogGPSLayout);
+        jDialogGPSLayout.setHorizontalGroup(
+            jDialogGPSLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanelGPS, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        jDialogGPSLayout.setVerticalGroup(
+            jDialogGPSLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanelGPS, javax.swing.GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE)
+        );
+
+        jDialogInter.setTitle("Position Capteur Intérieur");
+        jDialogInter.setMinimumSize(new java.awt.Dimension(500, 300));
+
+        jPanelInterMain.setLayout(new java.awt.GridLayout(1, 2));
+
+        jScrollPaneInterTree.setViewportView(jTreeInter);
+
+        jPanelInterMain.add(jScrollPaneInterTree);
+
+        jButtonInterOK.setText("OK");
+        jPanelInterMain.add(jButtonInterOK);
+
+        javax.swing.GroupLayout jDialogInterLayout = new javax.swing.GroupLayout(jDialogInter.getContentPane());
+        jDialogInter.getContentPane().setLayout(jDialogInterLayout);
+        jDialogInterLayout.setHorizontalGroup(
+            jDialogInterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanelInterMain, javax.swing.GroupLayout.DEFAULT_SIZE, 514, Short.MAX_VALUE)
+        );
+        jDialogInterLayout.setVerticalGroup(
+            jDialogInterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanelInterMain, javax.swing.GroupLayout.DEFAULT_SIZE, 302, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Connexion");
@@ -82,6 +170,11 @@ public class IData extends javax.swing.JFrame {
         jPanelButtonsLocalisation.add(jButtonInter);
 
         jButtonExter.setText("Capteur extérieur");
+        jButtonExter.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonExterActionPerformed(evt);
+            }
+        });
         jPanelButtonsLocalisation.add(jButtonExter);
 
         jPanelMain.add(jPanelButtonsLocalisation);
@@ -138,7 +231,8 @@ public class IData extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonInterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonInterActionPerformed
-
+        jDialogInter.setLocationRelativeTo(null);
+        jDialogInter.setVisible(true);
     }//GEN-LAST:event_jButtonInterActionPerformed
 
     private void jButtonNextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNextActionPerformed
@@ -147,23 +241,49 @@ public class IData extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jButtonNextActionPerformed
 
+    private void jButtonExterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonExterActionPerformed
+        jDialogGPS.setLocationRelativeTo(null);
+        jDialogGPS.setVisible(true);
+    }//GEN-LAST:event_jButtonExterActionPerformed
+
+    private void jButtonGPSOKActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonGPSOKActionPerformed
+        float latitude = Float.parseFloat(jTextFieldLat.getText());
+        float longitude = Float.parseFloat(jTextFieldLong.getText());
+        capteurExt = new PositionCapteurExt(latitude, longitude);
+        jLabelLocalisation.setText(jLabelLocalisation.getText() + " (" + capteurExt.toString() + ")");
+        jDialogGPS.dispose();
+    }//GEN-LAST:event_jButtonGPSOKActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonExter;
+    private javax.swing.JButton jButtonGPSOK;
     private javax.swing.JButton jButtonInter;
+    private javax.swing.JButton jButtonInterOK;
     private javax.swing.JButton jButtonNext;
     private javax.swing.JComboBox<String> jComboBoxType;
+    private javax.swing.JDialog jDialogGPS;
+    private javax.swing.JDialog jDialogInter;
     private javax.swing.JLabel jLabelID;
     private javax.swing.JLabel jLabelInterval;
+    private javax.swing.JLabel jLabelLat;
     private javax.swing.JLabel jLabelLocalisation;
+    private javax.swing.JLabel jLabelLong;
     private javax.swing.JLabel jLabelType;
     private javax.swing.JLabel jLabelUnit1;
     private javax.swing.JLabel jLabelUnit2;
     private javax.swing.JPanel jPanelBlank;
+    private javax.swing.JPanel jPanelBlank2;
     private javax.swing.JPanel jPanelButtonsLocalisation;
+    private javax.swing.JPanel jPanelGPS;
+    private javax.swing.JPanel jPanelInterMain;
     private javax.swing.JPanel jPanelMain;
     private javax.swing.JPanel jPanelSpinnersIntervals;
+    private javax.swing.JScrollPane jScrollPaneInterTree;
     private javax.swing.JSpinner jSpinnerMax;
     private javax.swing.JSpinner jSpinnerMin;
     private javax.swing.JTextField jTextFieldID;
+    private javax.swing.JTextField jTextFieldLat;
+    private javax.swing.JTextField jTextFieldLong;
+    private javax.swing.JTree jTreeInter;
     // End of variables declaration//GEN-END:variables
 }
---
+++ NeoCampus/src/ressources/PositionCapteurExt.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ressources;
+
+/**
+ *
+ * @author nicol
+ */
+public class PositionCapteurExt {
+    private final float latitude;
+    private final float longitude;
+    
+    
+    public PositionCapteurExt(float latitude, float longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+    
+    public float getLatitude() {
+        return this.latitude;
+    }
+    
+    public float getLongitude() {
+        return this.longitude;
+    }    
+    
+    
+    @Override
+    public String toString() {
+        return "Latitude : " + this.latitude + ", Longitude : " + this.longitude + "";
+    }
+}
